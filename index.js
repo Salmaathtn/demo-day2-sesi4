b8951f5cac823d0532b1b566f3a24fd36c3b8213
salma <salmaathoetani@gmail.com>
2024-12-08 22:45:27 +0700

    menambahkan repo index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import Animal from "./animal.js";
+import Mammal from "./mammal.js";
+
+const elephant = new Animal("Gajah", "Herbivora",true, 100, 200);
+const orangutan = new Mammal ("Tiger", "Carnivore", true, 50, 15);
+
+console.log(elephant)
+console.log(orangutan)

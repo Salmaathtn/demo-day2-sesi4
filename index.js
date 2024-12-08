6826fab6e64dd4c1bac9f55242c61f076b2eac3e
Salmaathtn <salmaathoetani@gmail.com>
2024-12-08 23:08:50 +0700

    menambahkan const tiger

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Mammal from "./mammal.js";
 
 const elephant = new Animal("Gajah", "Herbivora",true, 100, 200);
 const orangutan = new Mammal ("Tiger", "Carnivore", true, 50, 15);
+const tiger = new Animal("Harimau", "Karnivora", true, 100, 100);
 
 console.log(elephant)
 console.log(orangutan)
